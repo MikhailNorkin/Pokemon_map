26b54d1cad7b4a61fce1b96f07b0d7eb747fd1c9
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-09-11 10:44:07 +0300

    Added verbose_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,25 +2,27 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    description = models.TextField()
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    photo = models.ImageField(upload_to='poks', null=True, blank=True)
-    previous_evolution = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    """Покемон"""
+    description = models.TextField(verbose_name="описание:")
+    title_ru = models.CharField(max_length=200, verbose_name="на русском:")
+    title_en = models.CharField(max_length=200, blank=True, verbose_name="на английском:")
+    title_jp = models.CharField(max_length=200, blank=True, verbose_name="на японском:")
+    photo = models.ImageField(upload_to='poks', verbose_name="фото:", null=True, blank=True)
+    previous_evolution = models.ForeignKey("self", verbose_name="из кого родился", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.title_ru}'
     
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.CharField(max_length=10)
-    lon = models.CharField(max_length=10)
-    appeared_date = models.DateTimeField()
-    disappered_data = models.DateTimeField()
-    level = models.CharField(max_length=5)
-    health = models.CharField(max_length=5)
-    strength = models.CharField(max_length=5)
-    defence = models.CharField(max_length=5)
-    stamina = models.CharField(max_length=5)
+    """Покемон."""
+    pokemon = models.ForeignKey(Pokemon,  verbose_name="покемон", on_delete=models.CASCADE)
+    lat = models.CharField(max_length=10, verbose_name="широта")
+    lon = models.CharField(max_length=10, verbose_name="долгота")
+    appeared_date = models.DateTimeField(verbose_name="дата появления")
+    disappered_data = models.DateTimeField(verbose_name="дата изчезновения")
+    level = models.CharField(max_length=5, verbose_name="уровень")
+    health = models.CharField(max_length=5, verbose_name="здоровье")
+    strength = models.CharField(max_length=5, verbose_name="сила")
+    defence = models.CharField(max_length=5, verbose_name="защита")
+    stamina = models.CharField(max_length=5, verbose_name="стамина")
